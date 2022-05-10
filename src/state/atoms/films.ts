9ec70f390be35091851e6c6458154ccdeed352e3
Vinícius Romualdo <viniromualdo082@gmail.com>
2022-05-09 23:38:56 -0300

    refact atom/films.ts

--- src/state/atoms/films.ts
+++ src/state/atoms/films.ts
@@ -1,16 +1,18 @@
-import { IMovieDataInfo, IMovieHomeList, IMovieVideo } from '@types';
-import { atom } from 'recoil';
-import { heroFilmAsync, homeMovieListAsync } from '../selectors';
+// import { IMovieDataInfo, IMovieHomeList, IMovieVideo } from '@types';
+// import { atom } from 'recoil';
+// import { heroFilmAsync, homeMovieListAsync } from '../selectors';
 
-export const homeMovieListAtom = atom<IMovieHomeList[]>({
-	key: 'homeMovieList',
-	default: homeMovieListAsync,
-});
+// export const homeMovieListAtom = atom<IMovieHomeList[]>({
+// 	key: 'homeMovieList',
+// 	default: homeMovieListAsync,
+// });
 
-export const heroDataAtom = atom<{
-	video: IMovieVideo;
-	heroFilm: IMovieDataInfo;
-}>({
-	key: 'heroData',
-	default: heroFilmAsync,
-});
+// export const heroDataAtom = atom<{
+// 	video: IMovieVideo;
+// 	heroFilm: IMovieDataInfo;
+// }>({
+// 	key: 'heroData',
+// 	default: heroFilmAsync,
+// });
+
+export {};
